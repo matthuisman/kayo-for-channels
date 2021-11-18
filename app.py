0d4e6111904828ca3f4486a40fe61a8c437f6afa
Matt Huisman <gitlab@matthuisman.nz>
2021-11-18 15:57:43 +1300

    Add start_cho, include, exclude query params

--- app.py
+++ app.py
@@ -2,12 +2,16 @@
 import os
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from socketserver import ThreadingMixIn
+from urllib.parse import urlparse, parse_qsl
 
 import requests
 from kayo import Kayo
 
 PORT = 80
+USERNAME = os.getenv('USERNAME', '').strip()
+PASSWORD = os.getenv('PASSWORD', '').strip()
 CHUNKSIZE = 64 * 1024
+
 PLAYLIST_URL = 'playlist.m3u'
 EPG_URL = 'epg.xml'
 PLAY_URL = 'play'
@@ -15,6 +19,16 @@ STATUS_URL = ''
 EPG_SOURCE = 'https://i.mjh.nz/Kayo/epg.xml'
 
 class Handler(BaseHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        self._params = {}
+        super().__init__(*args, **kwargs)
+
+    def _error(self, message):
+        self.send_response(500)
+        self.end_headers()
+        self.wfile.write(f'Error: {message}'.encode('utf8'))
+        raise
+
     def do_GET(self):
         routes = {
             PLAYLIST_URL: self._playlist,
@@ -23,7 +37,10 @@ class Handler(BaseHTTPRequestHandler):
             STATUS_URL: self._status,
         }
 
-        func = self.path.split('/')[1]
+        parsed = urlparse(self.path)
+        func = parsed.path.split('/')[1]
+        self._params = dict(parse_qsl(parsed.query, keep_blank_values=True))
+
         if func not in routes:
             self.send_response(404)
             self.end_headers()
@@ -33,12 +50,6 @@ class Handler(BaseHTTPRequestHandler):
             routes[func]()
         except Exception as e:
             self._error(e)
-
-    def _error(self, message):
-        self.send_response(500)
-        self.end_headers()
-        self.wfile.write(f'Error: {message}'.encode('utf8'))
-        raise Exception(message)
 
     def _play(self):
         asset_id = self.path.split('/')[-1]
@@ -64,10 +75,28 @@ class Handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
 
+        start_chno = int(self._params['start_chno']) if 'start_chno' in self._params else None
+        include = [x for x in self._params.get('include', '').split(',') if x]
+        exclude = [x for x in self._params.get('exclude', '').split(',') if x]
+
         self.wfile.write(b'#EXTM3U\n')
         for row in kayo.live_channels():
-            self.wfile.write(u'#EXTINF:-1 tvg-id="{id}" tvg-chno="{channel}" channel-id="kayo-{id}" tvg-logo="{logo}",{name}\nhttp://{host}/{PLAY_URL}/{id}\n'.format(
-                id=row['asset']['id'], channel=row['chno'] or '', logo='{}?location=carousel-item&imwidth=415'.format(row['asset']['images']['defaultUrl']), name=row['asset']['title'], host=host, PLAY_URL=PLAY_URL).encode('utf8'))
+            channel_id = 'kayo-{}'.format(row['asset']['id'])
+            if (include and channel_id not in include) or (exclude and channel_id in exclude):
+                print(f"Skipping {channel_id} due to include / exclude")
+                continue
+
+            chno = ''
+            if start_chno is not None:
+                if start_chno > 0:
+                    chno = f' tvg-chno="{start_chno}"'
+                    start_chno += 1
+            elif row.get('chno') is not None:
+                chno = ' tvg-chno="{}"'.format(row['chno'])
+
+            self.wfile.write(u'#EXTINF:-1 channel-id="{channel_id}" tvg-id="{id}" tvg-logo="{logo}"{chno},{name}\nhttp://{host}/{PLAY_URL}/{id}\n'.format(
+                channel_id=channel_id, id=row['asset']['id'], logo='{}?location=carousel-item&imwidth=415'.format(row['asset']['images']['defaultUrl']), chno=chno,
+                    name=row['asset']['title'], host=host, PLAY_URL=PLAY_URL).encode('utf8'))
 
     def _status(self):
         self.send_response(200)
@@ -84,6 +113,6 @@ def run():
 
 if __name__ == '__main__':
     kayo = Kayo()
-    kayo.login(os.getenv('USERNAME', '').strip(), os.getenv('PASSWORD', '').strip())
+    kayo.login(USERNAME, PASSWORD)
     print("Starting server...")
     run()
